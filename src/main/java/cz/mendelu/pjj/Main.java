3a4bbf73a422f747f84b5de6d8fa94fd18932574
Andrii <blayson4@gmail.com>
2019-11-10 21:27:36 +0100

    Add Javadoc generated documentation

--- src/main/java/cz/mendelu/pjj/Main.java
+++ src/main/java/cz/mendelu/pjj/Main.java
@@ -1,5 +1,9 @@
 package cz.mendelu.pjj;
 
+/**
+ * @author Andrii But
+ * @author Olena Chuprina
+ */
 public class Main {
     public static void main(String[] args) {
         KeyMap keyMap = new KeyMap();
