969a90562eacae40894cdf9a047f1f6cb4b15ea2
Lena Chuprina <lena.chuprina.00@gmail.com>
2019-11-20 00:20:02 +0100

    Add equals() and hashCode()

--- src/main/java/cz/mendelu/pjj/Player.java
+++ src/main/java/cz/mendelu/pjj/Player.java
@@ -3,6 +3,8 @@ package cz.mendelu.pjj;
 import cz.mendelu.pjj.interfaces.Agent;
 import cz.mendelu.pjj.interfaces.PlayerInterface;
 
+import java.util.Objects;
+
 public class Player implements PlayerInterface {
 
     private String name;
@@ -53,5 +55,18 @@ public class Player implements PlayerInterface {
         if (checkWord(position)) {
             Agent agent = this.keyMap.getAgent(position);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Player)) return false;
+        Player player = (Player) o;
+        return name.equals(player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
